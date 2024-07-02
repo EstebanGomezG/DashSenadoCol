1e94dbb2e176fbf74454875f012d1dda352e20a7
Esteban Gómez <esteban6972@gmail.com>
2024-07-01 21:24:51 -0500

    DB a carpeta senado_col

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data():
-    df = pd.read_excel("Dash/Senado_Col.xlsx", sheet_name="dis_senado")
+    df = pd.read_excel("Dash/senado_col/Senado_Col.xlsx", sheet_name="dis_senado")
     df['foto'] = df['id'].apply(lambda x: f'/assets/{x}.jpg')
     print(df.columns)
     return df
